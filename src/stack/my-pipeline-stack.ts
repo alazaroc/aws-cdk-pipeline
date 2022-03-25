9dc967b2c7b7a39f6337ef361bc8a8c5ceffbfd6
alazaroc <alcalex@gmail.com>
2022-03-25 22:16:54 +0100

    retrieve github connection from ssm parameter store in my account

--- src/stack/my-pipeline-stack.ts
+++ src/stack/my-pipeline-stack.ts
@@ -1,4 +1,3 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {
   CodePipeline,
@@ -6,22 +5,22 @@ import {
   ShellStep,
 } from 'aws-cdk-lib/pipelines';
 import { MyPipelineAppStage } from './my-pipeline-app-stage';
+import { aws_ssm, Stack, StackProps } from 'aws-cdk-lib';
 
-export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MyPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const codePipelineName = `blog-infrastructure-cdk`;
+    const codePipelineName = `test-iac-with-cdk`;
     const pipeline = new CodePipeline(this, codePipelineName, {
       pipelineName: codePipelineName,
       synth: new ShellStep('Synth', {
-        // input: CodePipelineSource.gitHub('alazaroc/blog-infrastructure', 'main'),
+        // input: CodePipelineSource.gitHub('alazaroc/aws-cdk-pipeline', 'main'),
         input: CodePipelineSource.connection(
           'alazaroc/aws-cdk-pipeline',
           'main',
           {
-            connectionArn:
-              'arn:aws:codestar-connections:eu-west-1:139415085530:connection/fb936fb8-a047-43d5-90bd-129540d588e4', // Created using the AWS console * });',
+            connectionArn: getMyGitHubConnectionFromSsmParameterStore(this), // Created using the AWS console * });',
           },
         ),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
@@ -35,4 +34,14 @@ export class MyPipelineStack extends cdk.Stack {
       }),
     );
   }
+}
+
+// Get GitHub connection in a secure way from SSM Parameter Store
+export function getMyGitHubConnectionFromSsmParameterStore(
+  scope: Construct,
+): string {
+  return aws_ssm.StringParameter.valueForStringParameter(
+    scope,
+    'github-connection-alazaroc-cdkpipeline',
+  );
 }
